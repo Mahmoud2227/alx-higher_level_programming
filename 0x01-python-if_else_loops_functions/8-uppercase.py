0a1f8a20d05795ffe64e504f0e5c5fc642e6d6c2
Mahmoud Salah <mahmoud.salah2227@yahoo.com>
2022-09-20 22:56:22 +0200

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for i in str:
-        if ord(i) >= 97 and ord(i) <= 122:
-            print("{:c}".format(ord(i) - 32), end="")
-        else:
-            print("{:c}".format(ord(i)), end="")
