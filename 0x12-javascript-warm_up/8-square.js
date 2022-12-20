2fbabfb280c86a45b2a1ab2debea8cabf5f8338b
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-12-20 18:34:33 +0200

    fix formatting

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-let num = process.argv[2];
+const num = process.argv[2];
 
 if (isNaN(num)) {
-  console.log('Missing size');
+	console.log("Missing size");
 } else {
-  for (let i = 0; i < num; i++) {
-    let msg = '';
-    for (let j = 0; j < num; j++) {
-      msg = msg + 'X';
-    }
-    console.log(msg);
-  }
-}
+	for (let i = 0; i < num; i++) {
+		let msg = "";
+		for (let j = 0; j < num; j++) {
+			msg = msg + "X";
+		};
+		console.log(msg);
+	};
+};
